19ec949eaeb5ac124189e09d78a251decb325d8d
Hugo Sebesta <hugo.sebesta@gmail.com>
2024-04-09 16:45:52 +1000

    add basic project files

---
+++ Firmware/include/Algorithms.h
@@ -0,0 +1,18 @@
+// Algorithms.h
+//
+// Declarations of algorithms to be implemented
+//
+// DP Elective
+// Created 9/4/2024
+
+#ifndef ALGORITHMS_H
+#define ALGORITHMS_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+uint8_t pid_update(uint8_t sample_pin, char *thingo);
+
+uint8_t check_protection();
+
+#endif
